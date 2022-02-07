89711a285fe2fa44130eeb7efd112ca516ff84a7
wxamlzm <zd5024@163.com>
2022-02-07 18:19:36 +0800

    UPDATE：工厂模式雏形

---
+++ src/views/FactoryParrern/FactoryParren.vue
@@ -0,0 +1,25 @@
+<template>
+    <form action="">
+        <InputFactory v-for="item in inputList" :type="type" :status="status" :key="item"> </InputFactory>
+    </form>
+</template>
+
+<script>
+// 引入组件
+import InputFactory from './InputFactory';
+// 工厂模式练习
+export default {
+    name: 'Form',
+    props: {
+        type: '',
+    },
+    computed: {
+        inputList() {
+            return formMap[this.type];
+        },
+    },
+    components: {
+        InputFactory,
+    },
+};
+</script>
---
+++ src/views/FactoryParrern/InputFactory.vue
@@ -0,0 +1,34 @@
+<template>
+    <component :is="comInput" :type="type" :status="status"></component>
+</template>
+
+<script>
+import Username from './input/Username';
+import Mobile from './input/Mobile';
+import Email from './input/Email';
+import Password from './input/Password';
+import ImgCode from './input/ImgCode';
+
+const inputMap = {
+    Username,
+    Mobile,
+    Email,
+    Password,
+    ImgCode,
+};
+
+// input工厂
+export default {
+    props: {
+        name: '',
+        type: '',
+        status: '',
+    },
+    components: inputMap,
+    computed: {
+        comInput() {
+            return inputMap[this.name];
+        },
+    },
+};
+</script>
